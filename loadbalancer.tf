0dd4fde9e19396715b4b4800d139e45ad016148c
amangeldiyev <amangeldievaza@gmail.com>
2023-06-27 15:13:36 +0600

    Replace var.tfvars

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,22 +1,23 @@
 resource "yandex_lb_target_group" "slurm" {
   name      = "${local.prefix}-group"
-  region_id = "ru-central1-a"
+  region_id = "ru-central1"
   labels = var.labels
+
 
   target {
     subnet_id = "${yandex_vpc_subnet.lab-subnet-a.id}"
     address   = "${yandex_compute_instance.test.network_interface.0.ip_address}"
   }
 
-  target {
-    subnet_id = "${yandex_vpc_subnet.lab-subnet-a.id}"
-    address   = "${yandex_compute_instance.test-2.network_interface.0.ip_address}"
-  }
+#  target {
+#    subnet_id = "${yandex_vpc_subnet.lab-subnet-a.id}"
+#    address   = "${yandex_compute_instance.test-2.network_interface.0.ip_address}"
+#  }
 
-  target {
-    subnet_id = "${yandex_vpc_subnet.lab-subnet-a.id}"
-    address   = "${yandex_compute_instance.test-3.network_interface.0.ip_address}"
-  }
+#  target {
+#    subnet_id = "${yandex_vpc_subnet.lab-subnet-a.id}"
+#    address   = "${yandex_compute_instance.test-3.network_interface.0.ip_address}"
+#  }
 }
 
 
@@ -36,10 +37,11 @@ resource "yandex_lb_network_load_balancer" "slurm-balancer" {
     target_group_id = "${yandex_lb_target_group.slurm.id}"
 
     healthcheck {
-      name = "var.nlb_healthcheck.name"
+      #name = "var.nlb_healthcheck.name"
+      name = "test"
       http_options {
         port = var.nlb_healthcheck.port
-        path = "var.nlb_healthcheck.path"
+        path = "/"
       }
     }
   }
